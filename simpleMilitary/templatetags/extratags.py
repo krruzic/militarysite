f9cad62aa700e77a090babfb1494c95e92fbd129
krruzic <krruzic@gmail.com>
2015-03-29 21:37:02 -0600

    Table how we want it

--- simpleMilitary/templatetags/extratags.py
+++ simpleMilitary/templatetags/extratags.py
@@ -6,10 +6,10 @@ register = template.Library()
 # tag to get field's verbose name in template
 @register.simple_tag
 def get_verbose_name(object):
-    return object.verbose_name
+    return object.verbose_name.upper()
 
 # tag to get the value of a field by name in template
 @register.simple_tag
 def get_value_from_key(object, key):
     # is it necessary to check isinstance(object, dict) here?
-    return object[key.name]
+    return object[key]
--- simpleMilitary/views.py
+++ simpleMilitary/views.py
@@ -4,46 +4,23 @@ from django.template import RequestContext
 from django.template.response import TemplateResponse
 from simpleMilitary.models import Personnel
 from simpleMilitary.models import Person
-# Create your views here.
-#from django.utils
+
+
+
 import simplejson as json
 from django.core import serializers
 def index(request):
-   fields    = Personnel._meta.fields
-   data      = json.loads(serializers.serialize("json", Personnel.objects.all().order_by('-psin')))
+    names     = []
+    fields = {"First Name", "Last Name", "Base"}
+    for p in Personnel.objects.all().order_by('psin__fname'):
+        try:
+            bname = p.uid.bid.bname
+        except:
+            bname = "NULL"
+        names.append({"First Name": p.psin.fname, "Last Name": p.psin.lname, "Base": bname})
 
-   def parse_data(data):
-
-        result = []
-
-        # flatten the dictionary
-        def flatten_dict(d):
-            """
-            Because the only nested dict here is the fields, let's just
-            remove the 'fields' suffix so that the fields can be loaded in
-            template by name
-            """
-            def items():
-                for key, value in d.items():
-                    if isinstance(value, dict):
-                        for subkey, subvalue in flatten_dict(value).items():
-                            yield subkey, subvalue
-                    else:
-                        yield key, value
-
-            return dict(items())
-
-        for d in data:
-            # change the 'pk' key name into its actual name in the database
-            d[Personnel._meta.pk.name] = d.pop('pk')
-            # append the flattend dict of each object's field-value to the result
-            result.append(flatten_dict(d))
-
-        return result
-
-
-   context_instance = RequestContext(request, {
-       'data'      : parse_data(data),
-       'fields'    : fields,
-   })
-   return TemplateResponse(request, 'index.html', context_instance)
+    context_instance = RequestContext(request, {
+        'data'      : names,
+        'fields'    : fields,
+    })
+    return TemplateResponse(request, 'index.html', context_instance)
