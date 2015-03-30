77143e7a5075ef85163d4c9917b28df246fb6bbb
krruzic <krruzic@gmail.com>
2015-03-30 16:32:09 -0600

    oops

--- simpleMilitary/templatetags/extratags.py
+++ simpleMilitary/templatetags/extratags.py
@@ -11,9 +11,4 @@ register = template.Library()
 # tag to get the value of a field by name in template
 @register.simple_tag
 def get_value_from_key(object, key):
-<<<<<<< HEAD
-    # is it necessary to check isinstance(object, dict) here?
     return object[key]
-=======
-    return object[key]
->>>>>>> a9ad96fcc681d15937f98267eba4d98d3ced58fe
