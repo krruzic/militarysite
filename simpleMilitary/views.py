ed135f59e9f03be0b4609beab36ef9eed52d6e46
krruzic <krruzic@gmail.com>
2015-04-01 15:50:05 -0600

    Fixed views, and register page

--- simpleMilitary/views.py
+++ simpleMilitary/views.py
@@ -13,8 +13,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
 from django.core.context_processors import csrf
-
+from simpleMilitary.forms import RegistrationForm
 from django.core import serializers
+from django.contrib.auth.models import User
+
+
 
 def index(request):
     names     = []
@@ -37,29 +40,27 @@ def personnelDetail(request, personnel_sin):
         raise Http404("Personnel does not exist")
     return render(request, 'personnel/detail.html', {'personnel': personnel, 'weapons': weapons})
 
-def login_user(request):
-    c = {}
-    c.update(csrf(request))
-    state = "Please log in below..."
-    username = password = ''
-    if request.POST:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+# def login_user(request):
+#     state = "Please log in below..."
+#     username = password = ''
+#     if request.POST:
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
 
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                if request.GET.get('next', False):
-                    return HttpResponseRedirect(request.GET.get('next'))
-                else:
-                    return HttpResponseRedirect('/login/')
-            else:
-                state = "Your account is not active, please contact the site admin."
-        else:
-            state = "Your username and/or password were incorrect."
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 if request.GET.get('next', False):
+#                     return HttpResponseRedirect(request.GET.get('next'))
+#                 else:
+#                     return HttpResponseRedirect('/login/')
+#             else:
+#                 state = "Your account is not active, please contact the site admin."
+#         else:
+#             state = "Your username and/or password were incorrect."
 
-    return render(request, 'auth.html', {'state':state, 'username': username})
+#     return render(request, 'auth.html', {'state':state, 'username': username})
 
 
 def searchResults(request):
@@ -80,3 +81,23 @@ def searchResults(request):
         'fields'    : fields,
     })
     return TemplateResponse(request, 'index.html', context_instance)
+
+def register_page(request):
+    c = {}
+    error = ""
+    c.update(csrf(request))
+    print "TEST"
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        print "TEST2"
+        if form.is_valid():
+            print "Registered" + form.cleaned_data['username']
+            user = User.objects.create_user(
+                                            username=form.cleaned_data['username'],
+                                            password=form.cleaned_data['password1'],
+                                            )
+            return HttpResponseRedirect('/simpleMilitary/')
+    else:
+        form = RegistrationForm()
+        error = "Invalid registration form"
+    return render(request, 'register.html', {"form": form, "Error": error})
