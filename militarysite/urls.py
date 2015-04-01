7b1088b08b8d3d3cac1a065bd16e88ce9e7fe63a
krruzic <krruzic@gmail.com>
2015-04-01 16:04:25 -0600

    Add registration form

--- militarysite/urls.py
+++ militarysite/urls.py
@@ -7,11 +7,6 @@ from simpleMilitary import views
 urlpatterns = patterns('',
     url(r'^simpleMilitary/', include('simpleMilitary.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
-    url('^register/', CreateView.as_view(
-            template_name='register.html',
-            form_class=UserCreationForm,
-            success_url='/simpleMilitary/'
-    )),
     url('^accounts/', include('django.contrib.auth.urls')),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
     url(r'^admin/', include(admin.site.urls)),
--- simpleMilitary/admin.py
+++ simpleMilitary/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from simpleMilitary.models import *
 # Register your models here.
+class PersonnelAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Personal Information', {'fields': ['psin', 'supersin']}),
+        ('Service Information',  {'fields': ['rank', 'status', 'uid']}),
+    ]
+    search_fields = ['psin__fname', 'psin__lname']
+    list_display = ('get_fname', 'get_lname', 'get_sin')
+    list_filter = ['status']
+    actions = ['Personnel updated!']
+
+    def personnel_updated(self, request, queryset):
+        self.message_user(request, "Personnel successfully updated!")
+
+
 admin.site.register(Person)
+admin.site.register(Personnel, PersonnelAdmin)
