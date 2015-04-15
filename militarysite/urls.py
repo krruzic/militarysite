462ed4f81e969b77a33dcaa34bc418746f9d2ff1
krruzic <krruzic@gmail.com>
2015-04-15 02:19:38 -0600

    not sure what I did

--- militarysite/urls.py
+++ militarysite/urls.py
@@ -4,10 +4,12 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import admin
 from simpleMilitary import views
+from django.views.generic import RedirectView
 admin.site.login = views.login_user
 admin.site.logout = views.logout_user
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='simpleMilitary/')),
     url(r'^simpleMilitary/', include('simpleMilitary.urls', namespace="simpleMilitary")),
     url(r'^accounts/login/$', views.login_user),
     url(r'^accounts/logout/$', views.logout_user),
