205a963298090e072577461aebe18fd92a6f014d
krruzic <krruzic@gmail.com>
2015-04-19 14:41:01 -0600

    Updated admin inline to be disabled, work around weird bug

--- simpleMilitary/admin.py
+++ simpleMilitary/admin.py
@@ -2,38 +2,52 @@ from django.contrib import admin
 from simpleMilitary.models import *
 # Register your models here.
 class PersonnelAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('Personal Information', {'fields': ['psin', 'supersin']}),
-        ('Service Information',  {'fields': ['rank', 'status', 'uid']}),
-    ]
+    # Different form for change and add
+    def get_formsets(self, request, obj=None):
+        self.readonly_fields = None
+        self.change_fieldsets = [
+            ('Personal Information', {'fields': ['get_name', 'supersin']}),
+            ('Service Information',  {'fields': ['rank', 'status', 'uid']}),
+        ]
+        self.add_fieldsets = [
+            ('Personal Information', {'fields': ['psin', 'supersin']}),
+            ('Service Information',  {'fields': ['rank', 'status', 'uid']}),
+        ]
+        if obj is None:
+            self.fieldsets = self.add_fieldsets
+        else:
+            self.readonly_fields = ['get_name']
+            self.fieldsets = self.change_fieldsets
+        return super(PersonnelAdmin, self).get_formsets(request, obj)
     search_fields = ['psin__fname', 'psin__lname']
     list_display = ('get_fname', 'get_lname', 'get_sin')
     list_filter = ['status','rank']
-    actions = ['Personnel updated!']
 
-    def personnel_updated(self, request, queryset):
-        self.message_user(request, "Personnel successfully updated!")
-    class Media:
-        js = ("/static/js/hide_mypersonnel_info.js",)
+    # class Media:
+    #     js = ("/static/js/hide_mypersonnel_info.js",)
 
-    # print submit_buttons_top
-
-class PersonAdmin(admin.ModelAdmin):
-    def get_model_perms(self, request):
-        return {}
 
 
 
 
 class PersonnelInline(admin.TabularInline):
     model = Personnel
-    verbose_name_plural = "Members"
-    readonly_fields = ('psin', 'rank', 'status', 'supersin',)
+    verbose_name_plural = 'Members'
     extra = 0
+
     def has_delete_permission(self, request, obj=None):
         return False
     def has_add_permission(self, request, obj=None):
         return False
+
+    class Media:
+        js = ("/static/js/disable_inline_info.js",)
+
+class PersonAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        return {}
+    verbose_name_plural = 'Name'
+    verbose_name = 'Name'
 
 class UnitAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -49,12 +63,15 @@ class UnitAdmin(admin.ModelAdmin):
 
 class UnitInline(admin.TabularInline):
     model = Unit
-    readonly_fields = ('uname', 'type', 'get_cname', 'cid', 'commander_sin', 'uid')
     extra = 0
     def has_delete_permission(self, request, obj=None):
         return False
     def has_add_permission(self, request, obj=None):
         return False
+
+    class Media:
+        js = ("/static/js/disable_inline_info.js",)
+
 
 class BaseAdmin(admin.ModelAdmin):
     search_fields = ['bname','location']
